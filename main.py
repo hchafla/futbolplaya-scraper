760313694066828e689328a8739e49cae89dbb68
hchafla <161073227+hchafla@users.noreply.github.com>
2026-08-18 18:03:58 +0100

    Add datetime and write to noticias.json

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-1
+from datetime import datetime
+
+print("Hola desde GitHub Actions")
+
+with open("data/noticias.json", "w", encoding="utf-8") as f:
+    f.write(
+        '{"actualizado":"' + datetime.now().isoformat() + '"}'
+    )
